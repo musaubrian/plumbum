3a826fbbd657a2760592bd09ce6c0eede4979ee4
musaubrian <musaubrian15@gmail.com>
2022-11-04 10:16:19 +0300

    chore: initialize script

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+check if the plumbum package has less
+security risks than subprocess module
+"""
+import plumbum
+
+
+def check_plum():
+    pass
+
+
+if __name__ == "__main__":
+    check_plum()
